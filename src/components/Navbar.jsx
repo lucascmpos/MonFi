3d24267f19339a12d9daf650b62f332fbab433f5
lucascmpos <lucasoliveiracampos81@gmail.com>
2023-05-15 17:46:34 -0300

    Projeto finalizado, faltando apenas alguns ajustes css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
-
-
-
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
   return (
-    <div>
-      <nav id="navbar">
-        <h2>
-          <Link to="/">
-            <BiCameraMovie />
-            MonFi
-          </Link>
-        </h2>
-        <form>
-          <input type="text" placeholder="Busque um filme" />
-          <button type="submit">
-            <BiSearchAlt2 />
-          </button>
-        </form>
-      </nav>
-    </div>
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MonFi
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 
+
+import './MovieGrid.css'
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
